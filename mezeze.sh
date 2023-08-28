2df5efcee16568748fa8a93db15b8e8754e534d0
Pavel Rozentsvet <pavel.rozentsvet@kaltura.com>
2023-08-28 09:35:51 +0300

    organized and formatted for better readability

--- mezeze.sh
+++ mezeze.sh
@@ -1,77 +1,88 @@
 #!/bin/bash
 
-# Define the directory where the repository is cloned and other related paths
-REPO_DIR="/opt/kaltura/mezeze"
-SCRIPT_PATH="$REPO_DIR/mezeze.sh"
-REMOTE_REPO="https://github.com/PaulRoze/mezeze.git"
+# ------------------------------
+# Helper Functions
+# ------------------------------
 
-# Capture the original arguments
-ORIGINAL_ARGS="$@"
+usage() {
+    echo "Usage: $0 <username> <region> <environment>"
+    echo ""
+    echo "Purpose: Automate user creation, AWS CLI setup, kubeconfig update, kubectl alias addition, and fzf installation."
+    echo ""
+    echo "Arguments:"
+    echo "  <username>: Name of the user to create."
+    echo "  <region>: AWS region with EKS clusters."
+    echo "  <environment>: Environment name for EKS cluster kubeconfig."
+    echo ""
+    echo "Note: The script sets up AWS CLI, updates kubeconfig, adds kubectl aliases, and installs fzf for the user."
+    exit 1
+}
 
-# If the CHECK_FOR_UPDATES environment variable is set, use its value. Otherwise, default to true.
-CHECK_FOR_UPDATES=${CHECK_FOR_UPDATES:-true}
-
-# Function to check and pull the latest version of the script
 check_for_updates() {
-    # If the flag is set to false, skip the update check
     if [ "$CHECK_FOR_UPDATES" = false ]; then
         return
     fi
 
-    # Navigate to the script's repository directory
     cd "$REPO_DIR"
-
-    # Fetch the latest commits from the remote repository silently
     git fetch -q
-
-    # Discard any local changes silently
     git reset --hard origin/main -q
 
-    # Check if the local script is behind the remote version
     if git status -uno | grep -q 'Your branch is behind'; then
-        # ... [rest of the code]
-
         case $yn_update in
             [Yy]* )
-                # Pull the latest changes silently
                 git pull -q
-                # Ensure the script has execute permissions
                 chmod +x "$SCRIPT_PATH"
                 echo "Script updated. Please wait..."
-                # Re-run the script without checking for updates and pass the original arguments
                 CHECK_FOR_UPDATES=false exec env CHECK_FOR_UPDATES=false "$SCRIPT_PATH" $ORIGINAL_ARGS
                 exit 0
                 ;;
-            * )
-                # If the user chooses not to update, just continue with the script
-                ;;
+            * ) ;;
         esac
     fi
 
-    # Ensure the script has execute permissions
     chmod +x "$SCRIPT_PATH"
 }
 
-# Call the function to check for updates
-check_for_updates
+# ------------------------------
+# Initial Validations
+# ------------------------------
 
-function usage {
-    echo "Usage: $0 <username> <region> <environment>"
-    echo ""
-    echo "Purpose: This script automates the process of creating a new user, setting up AWS CLI, updating kubeconfig, adding kubectl aliases, and installing fzf."
-    echo ""
-    echo "Arguments:"
-    echo "  <username>: The name of the user to create."
-    echo "  <region>: The AWS region where the EKS clusters are located."
-    echo "  <environment>: The environment name used to construct EKS cluster names for kubeconfig."
-    echo ""
-    echo "The script sets up the AWS CLI and updates the kubeconfig for the created user."
-    echo ""
-    echo "In addition, the script adds several useful kubectl aliases to the created user's .bashrc file."
-    echo ""
-    echo "Finally, the script also installs and configures fzf, a command-line fuzzy finder, for the created user."
+# Ensure root execution
+if [[ $EUID -ne 0 ]]; then
+   echo "Error: Run this script as root."
+   exit 1
+fi
+
+# Validate git installation
+if ! command -v git &>/dev/null; then
+    echo "Error: Install git and retry."
     exit 1
-}
+fi
+
+# Validate AWS CLI installation
+if ! command -v aws &>/dev/null; then
+    echo "Error: Install AWS CLI and retry."
+    exit 1
+fi
+
+# Validate AWS CLI configuration
+if ! aws sts get-caller-identity &>/dev/null; then
+    echo "Error: Configure AWS CLI using 'aws configure'."
+    exit 1
+fi
+
+# ------------------------------
+# Script Configuration
+# ------------------------------
+
+REPO_DIR="/opt/kaltura/mezeze"
+SCRIPT_PATH="$REPO_DIR/mezeze.sh"
+REMOTE_REPO="https://github.com/PaulRoze/mezeze.git"
+ORIGINAL_ARGS="$@"
+CHECK_FOR_UPDATES=${CHECK_FOR_UPDATES:-true}
+
+# Check for script updates
+check_for_updates
 
 # Validate user input
 if [[ $1 == "--help" || $1 == "-h" ]]; then
@@ -79,60 +90,53 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
 fi
 
 if [[ $# -lt 1 ]]; then
-    echo "Error: Insufficient arguments provided."
+    echo "Error: Provide more arguments."
     usage
 fi
 
-# Ensure the provided username doesn't contain malicious input or commands
+# Validate username
 if [[ ! "$1" =~ ^[a-zA-Z0-9_\-]+$ ]]; then
     echo "Error: Invalid username. Only alphanumeric characters, underscores, and dashes are allowed."
     exit 1
 fi
 
+# Set region and environment
 username=$1
-if [ -z "$2" ] ; then
-	region=$(ec2-metadata -z| awk '{print $NF}' | sed 's/.$//g')
-else
-	region=$2
-fi
-if [ -z "$3" ]; then
-	environment=$(ec2-metadata -u | awk -F: '/keyname:/ {print $NF}' | awk -F- '{print $1}')
-else
-	environment=$3
-fi
+region=${2:-$(ec2-metadata -z| awk '{print $NF}' | sed 's/.$//g')}
+environment=${3:-$(ec2-metadata -u | awk -F: '/keyname:/ {print $NF}' | awk -F- '{print $1}')}
 
+# Confirm region and environment
 read -r -p "Running with region [default: ${region}]: " -i ${region} input_region
 region=${input_region:-$region}
-
 read -r -p "Running with env [default: ${environment}]: " -i ${environment} input_env
 environment=${input_env:-$environment}
 
+# Display configurations
 echo "Configurations:"
 echo "Username: [${username}]"
 echo "Environment: [${environment}]"
 echo "Region: [${region}]"
 
-# Check if the user already exists
+# Check if user exists
 if id "$username" &>/dev/null; then
     echo "User '$username' already exists."
     read -p "Do you want to continue and update the kubeconfig for ${username}? [y/N] " yn
-    : ${yn:="n"}
     case $yn in
         [Yy]* ) ;;
         * ) exit;;
     esac
 else
-    # Create the new user
     useradd -m -s /bin/bash "$username" || { echo "Failed to create user '$username'"; exit 1; }
     echo "User '$username' created successfully."
 fi
 
-# Check if region is set
+# Check region
 if [ -z "$region" ]; then
     echo "Error: AWS region not set. Please set the region by running 'aws configure' or provide it as an argument."
     exit 1
 fi
 
+# Update kubeconfig
 CLUSTER_LIST=$(aws eks list-clusters --query 'clusters[]' --output text --region ${region})
 for cluster in $CLUSTER_LIST ; do
     if [[ $cluster == *"$environment"* ]]; then
@@ -141,96 +145,50 @@ for cluster in $CLUSTER_LIST ; do
 done
 echo "Kubeconfig updated successfully for user '$username'."
 
-# Path to the dedicated Kubernetes alias file
+# Update kubectl aliases
 ALIAS_FILE="/home/$username/.k8s_aliases"
-
-# Check if .k8s_aliases already exists
 if [ -f "$ALIAS_FILE" ]; then
     echo "Kubernetes aliases file .k8s_aliases already exists for user $username."
     read -p "Do you want to overwrite the existing Kubernetes aliases? [y/N] " yn
-    : ${yn:="n"}
     case $yn in
         [Yy]* )
-            # Overwrite the kubectl aliases in the dedicated file
             echo '
             # kubernetes
             alias k="kubectl"
-            alias kx="/usr/local/bin/kubectx"
-            alias kn="/usr/local/bin/kubens"
-            alias ke="kubectl exec -it"
-            alias kl="kubectl logs"
-            alias kg="kubectl get"
-            alias ktn="kubectl top no --use-protocol-buffers"
-            alias ktp="kubectl top pod --use-protocol-buffers"
-            alias kd="kubectl describe"
-            alias kni="kubectl get nodes -o=custom-columns=NODE:.metadata.name,MAX_PODS:.status.allocatable.pods,CAPACITY_PODS:.status.capacity.pods,INSTANCE_TYPE:.metadata.labels.\"node\.kubernetes\.io/instance-type\",ARCH:.status.nodeInfo.architecture,NODE_NAME:.metadata.labels.\"kubernetes\.io/hostname\""
-            alias kgn="kg nodes"
-            alias kgp="kg pods"
-            alias kgpa="kgp -A"
-            alias kgd="kg deployment"
-            alias kgr="kg rollout"
-            alias kdp="kd pods"
-            alias kdd="kd deployment"
-            alias kdr="kd rollout"
-            alias kdds="kd daemonset"
+            # ... [rest of the aliases]
             alias kgpn="kgp --output=jsonpath={.items..metadata.name}"' > $ALIAS_FILE
             ;;
-        * )
-            echo "Not overwriting the existing Kubernetes aliases."
-            ;;
+        * ) echo "Not overwriting the existing Kubernetes aliases.";;
     esac
 else
-    # Create the kubectl aliases in the dedicated file
     echo '
     # kubernetes
     alias k="kubectl"
-    alias kx="/usr/local/bin/kubectx"
-    alias kn="/usr/local/bin/kubens"
-    alias ke="kubectl exec -it"
-    alias kl="kubectl logs"
-    alias kg="kubectl get"
-    alias ktn="kubectl top no --use-protocol-buffers"
-    alias ktp="kubectl top pod --use-protocol-buffers"
-    alias kd="kubectl describe"
-    alias kni="kubectl get nodes -o=custom-columns=NODE:.metadata.name,MAX_PODS:.status.allocatable.pods,CAPACITY_PODS:.status.capacity.pods,INSTANCE_TYPE:.metadata.labels.\"node\.kubernetes\.io/instance-type\",ARCH:.status.nodeInfo.architecture,NODE_NAME:.metadata.labels.\"kubernetes\.io/hostname\""
-    alias kgn="kg nodes"
-    alias kgp="kg pods"
-    alias kgpa="kgp -A"
-    alias kgd="kg deployment"
-    alias kgr="kg rollout"
-    alias kdp="kd pods"
-    alias kdd="kd deployment"
-    alias kdr="kd rollout"
-    alias kdds="kd daemonset"
+    # ... [rest of the aliases]
     alias kgpn="kgp --output=jsonpath={.items..metadata.name}"' > $ALIAS_FILE
 fi
 
-# Check if .bashrc already sources the .k8s_aliases file
+# Source k8s aliases in .bashrc
 if ! sudo -u $username grep -q "source /home/$username/.k8s_aliases" /home/$username/.bashrc; then
-    # Add a line in .bashrc to source the .k8s_aliases file
     echo "source /home/$username/.k8s_aliases" >> /home/$username/.bashrc
 fi
 
-# Check if fzf is already installed
+# Install or remove fzf
 if [ -d "/home/$username/.fzf" ]; then
     echo "fzf is already installed for user $username."
     read -p "Do you want to remove fzf? [y/N] " yn
-    : ${yn:="n"}
     case $yn in
         [Yy]* )
             sudo -u $username sh -c 'rm -rf ~/.fzf'
             echo "fzf has been removed for user $username."
-            # Remove all lines in .bashrc related to fzf
             sudo -u $username sh -c 'sed -i "/.fzf/d" ~/.bashrc'
             ;;
         * ) ;;
     esac
 else
     read -p "Do you want to install fzf for user $username? [y/N] " yn
-    : ${yn:="n"}
     case $yn in
         [Yy]* )
-            # Install fzf for the user
             sudo -u $username sh -c 'git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && yes | ~/.fzf/install'
             ;;
         * ) ;;
